f53cdd1bfc44594f6672c65719d10917890769da
maojui <maojui0427@gmail.com>
2019-06-14 00:22:41 +0800

    Update 2019/06/14 00:22:41

--- 0ctf2019/final/Quantum Game/solve.py
+++ 0ctf2019/final/Quantum Game/solve.py
@@ -2,25 +2,27 @@ from pwn import *
 import numpy as np
 
 conn = remote('127.0.0.1',13337)
+
 # Stage 1
 conn.recvuntil('Your turn:')
 conn.recvuntil('\n')
 conn.sendline('H 0')
 conn.sendline('H 0\nX 0')
 print("Stage 1 clear.")
+
 # Stage 2
 conn.recvuntil('Your turn:')
 conn.recvuntil('\n')
-conn.sendline('H 0\nX 0\nH 0')
+conn.sendline('Y 0')
 print("Stage 2 clear.")
-# conn.interactive()
+
 # Stage 3
 conn.recvuntil('(get ')
 num = int(conn.recvuntil(' ').strip())
 conn.recvuntil('toss the coin? ')
 conn.recvuntil('\n')
 conn.sendline(f'RX({np.arcsin(np.sqrt(num / 100000)) * 2}) 0')
-print("Stage 3 clear.")
+print("Stage 3 ...")
 
 conn.interactive()
 conn.close()
